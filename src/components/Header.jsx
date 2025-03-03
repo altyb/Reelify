ccdf4bb2ad8fdc6c660ca1f49fb30dace511407d
altyb <altybanony@gmail.com>
2025-03-03 09:14:30 +0200

    Change app name from Flixsy to Reelify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
-// ... existing imports ...
+import { Link } from "react-router-dom";
 import LanguageSwitcher from './LanguageSwitcher';
 
-function Header() {
+export const Header = () => {
   return (
-    <header className="bg-black text-white p-4 flex justify-between items-center">
-      <div className="flex items-center">
-        <h1 className="text-2xl font-bold">Flixsy</h1>
-      </div>
-      
-      {/* Add Language Switcher here */}
-      <div className="flex items-center">
-        <LanguageSwitcher />
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="flex items-center gap-2">
+          <h1 className="text-2xl font-bold">Reelify</h1>
+        </Link>
+        <nav className="flex gap-4">
+          <Link to="/" className="hover:text-blue-300">Home</Link>
+          <Link to="/movies" className="hover:text-blue-300">Movies</Link>
+          <Link to="/tv" className="hover:text-blue-300">TV Shows</Link>
+          <Link to="/search" className="hover:text-blue-300">Search</Link>
+        </nav>
+        <div className="flex items-center">
+          <LanguageSwitcher />
+        </div>
       </div>
     </header>
   );
-}
-
-export default Header;
+};
